cf12dfed9610141480557f8cb340507362de9aca
maligeez <anonysss11@gmail.com>
2024-02-28 20:16:18 +0100

    TAILWIND

---
+++ my-app/src/components/Todo.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function Todo() {
+  return (
+    <>
+        <div className='flex '>
+            <div className='flex flex-row'>
+                {/*to do*/}
+                <div>
+    f
+                </div>
+                {/*doing*/}
+                <div>
+                    f
+                </div>
+                {/*done*/}
+                <div>
+                   f 
+                </div>
+            </div>
+        </div>
+    
+    
+    
+    </>
+  )
+}
+
+export default Todo
